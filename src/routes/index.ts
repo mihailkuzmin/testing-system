aa48d4a655bc999987a1b345dd10929aeae49c87
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-15 20:36:22 +0300

    auth.controller stub

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ export const routes: IController = (app, options, done) => {
   app.register(controllers.studentController, { prefix: '/student' })
   app.register(controllers.taskController, { prefix: '/task' })
   app.register(controllers.groupController, { prefix: '/group' })
+  app.register(controllers.authController, { prefix: '/auth' })
 
   done()
 }
