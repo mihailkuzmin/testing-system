2b4026a188e195f2b05f1f904fee9ff546140e52
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-21 18:23:48 +0300

    Add fade in animation for AddModal

--- client/src/pages/Users/AddModal/AddModal.tsx
+++ client/src/pages/Users/AddModal/AddModal.tsx
@@ -8,6 +8,8 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
+  Fade,
+  Backdrop,
 } from '@material-ui/core'
 import styles from './AddModal.module.css'
 
@@ -18,31 +20,42 @@ interface IAddModalProps {
 
 export const AddModal = ({ open, onClose }: IAddModalProps) => {
   return (
-    <Modal className={styles.modal} open={open} onClose={onClose}>
-      <Paper className={styles.content}>
-        <h3>Добавить пользователя</h3>
-        <form noValidate autoComplete='off'>
-          <div className={styles.formContainer}>
-            <TextField label='ФИО' variant='outlined' />
-            <FormControl variant='outlined'>
-              <InputLabel id='group-select-label'>Группа</InputLabel>
-              <Select labelId='group-select' id='group-select' value='' label='Группа'>
-                <MenuItem disabled value=''>
-                  Группа
-                </MenuItem>
-                <MenuItem value={10}>АП-31</MenuItem>
-                <MenuItem value={20}>АП-21</MenuItem>
-                <MenuItem value={30}>АП-11</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField label='Логин' variant='outlined' />
-            <TextField label='Пароль' variant='outlined' />
-          </div>
-        </form>
-        <Button variant='contained' color='primary'>
-          Добавить
-        </Button>
-      </Paper>
+    <Modal
+      className={styles.modal}
+      open={open}
+      onClose={onClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <Paper className={styles.content}>
+          <h3>Добавить пользователя</h3>
+          <form noValidate autoComplete='off'>
+            <div className={styles.formContainer}>
+              <TextField label='ФИО' variant='outlined' />
+              <FormControl variant='outlined'>
+                <InputLabel id='group-select-label'>Группа</InputLabel>
+                <Select labelId='group-select' id='group-select' value='' label='Группа'>
+                  <MenuItem disabled value=''>
+                    Группа
+                  </MenuItem>
+                  <MenuItem value={10}>АП-31</MenuItem>
+                  <MenuItem value={20}>АП-21</MenuItem>
+                  <MenuItem value={30}>АП-11</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField label='Логин' variant='outlined' />
+              <TextField label='Пароль' variant='outlined' />
+            </div>
+          </form>
+          <Button variant='contained' color='primary'>
+            Добавить
+          </Button>
+        </Paper>
+      </Fade>
     </Modal>
   )
 }
