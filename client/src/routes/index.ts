4c6424a4c14eebb7dad27ce5619547d2fba7d2d6
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-21 13:45:25 +0300

    New routes config

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,19 +1,25 @@
-import { Main } from '../pages'
-import * as Edit from '../pages/Edit'
+import * as Pages from '../pages'
 import { IRoutesConfig } from '../typings'
+
+/**
+ * Use another groupName for group links in dropdown menu
+ */
 
 export const routesConfig: IRoutesConfig = {
   admin: [
-    { groupName: 'Single', routes: [{ path: '/', title: 'Main', Page: Main }] },
     {
-      groupName: 'Edit',
-      routes: [{ path: 'edit/users', title: 'Users', Page: Edit.Users }],
+      groupName: 'Single',
+      routes: [
+        { path: '/', title: 'Главная', Page: Pages.Main },
+        { path: '/groups', title: 'Группы', Page: Pages.Groups },
+        { path: '/users', title: 'Пользователи', Page: Pages.Users },
+      ],
     },
   ],
   common: [
     {
       groupName: 'Single',
-      routes: [{ path: '/', title: 'Main', Page: Main }],
+      routes: [{ path: '/', title: 'Main', Page: Pages.Main }],
     },
   ],
 }
