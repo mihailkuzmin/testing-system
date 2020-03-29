1d9144f8a253a44194c51875e42fc1a4714d2671
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-29 12:28:12 +0300

    Add Works page stub

---
+++ client/src/pages/Works/Works.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { IPageProps } from '../../typings'
+import { Layout } from '../../components'
+
+export const Works = (props: IPageProps) => {
+  return <Layout>Works</Layout>
+}
