2b8c392b5a31576818c6cd32a3b4db0605ab3f58
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-29 10:33:28 +0300

    Add Tasks page stub

---
+++ client/src/pages/Tasks/Tasks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {IPageProps} from '../../typings'
+import {Layout} from '../../components'
+
+export const Tasks = (props: IPageProps) => {
+  return (
+    <Layout>
+      Tasks
+    </Layout>
+  )
+}
