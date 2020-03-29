eacdef9fd05978cfcc829ae552ab980869f9e4bf
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-29 10:35:39 +0300

    Add Tasks icon

---
+++ client/src/components/Icons/Tasks.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+
+export const Tasks = () => <FormatListNumberedIcon />
--- client/src/components/Icons/index.ts
+++ client/src/components/Icons/index.ts
@@ -1,3 +1,4 @@
 export { Home } from './Home'
 export { Groups } from './Groups'
 export { Users } from './Users'
+export {Tasks} from './Tasks'
