b8edd96bc15d42257ebb69ba21807af219d6e720
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-29 10:24:13 +0300

    Remove reexport from components/Buttons

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,13 +3,6 @@ export { LinksGroup } from './LinksGroup'
 export { Layout } from './Layout'
 export { Modal } from './Modal'
 export { ModalActions } from './ModalActions'
-export { TableRowActions } from './TableRowActions'
 export { TableTitle } from './TableTitle'
 export { TableHeader } from './TableHeader'
 export { TableHeaderActions } from './TableHeaderActions'
-export {
-  PrimaryButton,
-  SecondaryButton,
-  EditButton,
-  DeleteButton,
-} from './Buttons'
