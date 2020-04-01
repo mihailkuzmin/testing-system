89284e2081e41feb1a835143ae983e7455a68b39
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-01 12:18:28 +0300

    Add filter to incoming messages. Message will be created only if it has text

--- client/src/model/events.ts
+++ client/src/model/events.ts
@@ -2,22 +2,22 @@ import { createEvent } from 'effector'
 import { NewMessage, Message } from '../typings'
 import { nanoid } from 'nanoid'
 
-export const newMessage = createEvent<Message>()
-
-export const createMessage = newMessage.prepend(
-  ({ type, text }: NewMessage) => {
-    return {
-      key: nanoid(),
-      message: text,
-      options: {
-        autoHideDuration: 2000,
-        disableWindowBlurListener: true,
-        variant: type,
-      },
-      displayed: false,
-    }
-  },
-)
+export const createMessage = createEvent<NewMessage>()
+export const messageWithText = createMessage.filter({
+  fn: (message) => Boolean(message.text),
+})
+export const newMessage = messageWithText.map<Message>(({ type, text }) => {
+  return {
+    key: nanoid(),
+    message: text,
+    options: {
+      autoHideDuration: 2000,
+      disableWindowBlurListener: true,
+      variant: type,
+    },
+    displayed: false,
+  }
+})
 
 export const setDisplayed = createEvent<string>()
 export const removeMessage = createEvent<string>()
