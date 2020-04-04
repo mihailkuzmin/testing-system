30c43ed09269705baaa60a35021dd3033f880818
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-04 13:29:37 +0300

    Add onClick prop

--- client/src/components/Buttons/DeleteButton.tsx
+++ client/src/components/Buttons/DeleteButton.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { IconButton } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/DeleteOutlineOutlined'
 
-export const DeleteButton = () => {
+interface IDeleteButtonProps {
+  onClick?: any
+}
+
+export const DeleteButton = (props: IDeleteButtonProps) => {
   return (
-    <IconButton size='small'>
+    <IconButton onClick={props.onClick} size='small'>
       <DeleteIcon />
     </IconButton>
   )
--- client/src/components/Buttons/EditButton.tsx
+++ client/src/components/Buttons/EditButton.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { IconButton } from '@material-ui/core'
 import EditIcon from '@material-ui/icons/EditOutlined'
 
-export const EditButton = () => {
+interface IEditButtonProps {
+  onClick?: any
+}
+
+export const EditButton = (props: IEditButtonProps) => {
   return (
-    <IconButton size='small'>
+    <IconButton onClick={props.onClick} size='small'>
       <EditIcon />
     </IconButton>
   )
