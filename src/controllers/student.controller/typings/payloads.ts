742b12da6c706c178df7dc7c2c49d06902a5b4ed
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-04 12:38:12 +0300

    Create payload & message for removeById() query

--- src/controllers/student.controller/typings/payloads.ts
+++ src/controllers/student.controller/typings/payloads.ts
@@ -3,3 +3,4 @@ import { StudentQueryResult } from '../../../typings/queries/student'
 export interface Create extends StudentQueryResult {}
 export interface GetAll extends Array<StudentQueryResult> {}
 export interface GetById extends StudentQueryResult {}
+export interface RemoveById extends StudentQueryResult {}
