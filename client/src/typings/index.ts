07d7cf57724922d930ed962432095a87d4684299
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-06 14:54:31 +0300

    add put method to Request interface

--- client/src/typings/index.ts
+++ client/src/typings/index.ts
@@ -41,6 +41,7 @@ export enum Status {
 export interface Request {
   get: <R>(url: string) => Promise<Response<R>>
   post: <P, R>(url: string, payload?: P) => Promise<Response<R>>
+  put: <P, R>(url: string, payload?: P) => Promise<Response<R>>
   delete: <P, R>(url: string, payload?: P) => Promise<Response<R>>
 }
 
