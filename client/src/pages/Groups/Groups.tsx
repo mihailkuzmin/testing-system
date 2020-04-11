f92784bcdbe92417a365371b366c3c50e1596534
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-11 18:39:25 +0300

    add GroupsPage mount event

--- client/src/pages/Groups/Groups.tsx
+++ client/src/pages/Groups/Groups.tsx
@@ -3,9 +3,11 @@ import { useStore } from 'effector-react'
 import { IPageProps } from '../../typings'
 import { GroupsTable } from './GroupsTable'
 import { Layout } from '../../components'
-import { groupsTable } from './model'
+import { groupsTable, GroupsPage } from './model'
 
 export const Groups = (props: IPageProps) => {
+  React.useEffect(GroupsPage.onMount, [])
+
   const groups = useStore(groupsTable.$groups)
 
   return (
--- client/src/pages/Groups/model/index.ts
+++ client/src/pages/Groups/model/index.ts
@@ -1,3 +1,4 @@
 export { addForm } from './addForm'
 export { addModal } from './addModal'
 export { groupsTable } from './groupsTable'
+export { GroupsPage } from './page'
