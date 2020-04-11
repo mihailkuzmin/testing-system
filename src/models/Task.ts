235d49f55b95bd557db11522be8970f9fbe68d6f
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-11 17:24:07 +0300

    queries refactoring

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -2,18 +2,15 @@ import { db } from '../db'
 import { TaskQueryResult, CreateTask } from '../typings/task'
 
 export class Task {
-  constructor(
-    private description: string,
-    private exampleInput: string,
-    private exampleOutput: string,
-    private correctOutput: string,
-  ) {}
-
   static async getById(id: number | string) {
     try {
       const [result] = await db.query(
         `
-        SELECT * FROM Task WHERE Task.id = ($1)
+        SELECT
+          T.id, T.description, T.example_input as "exampleInput",
+          T.example_output as "exampleOutput", T.correct_output as "correctOutput"
+        FROM Task T
+        WHERE T.id = ($1)
       `,
         [id],
       )
@@ -26,7 +23,10 @@ export class Task {
   static async getAll(): Promise<TaskQueryResult[]> {
     try {
       const result = await db.query(`
-        SELECT * from Task
+        SELECT
+          T.id, T.description, T.example_input as "exampleInput",
+          T.example_output as "exampleOutput", T.correct_output as "correctOutput"
+        from Task T
       `)
       return result
     } catch (e) {
