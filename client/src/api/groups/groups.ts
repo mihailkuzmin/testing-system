fe681d88c3969782f25f2b43d87bcb848f82e444
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-12 10:24:56 +0300

    implement create method

--- client/src/api/groups/groups.ts
+++ client/src/api/groups/groups.ts
@@ -1,12 +1,18 @@
 import { request } from '../request'
 import { Response } from '../../typings'
-import { Group } from './typings'
+import { Group, CreateGroup } from './typings'
 
 const getAll = async (): Promise<Response<Group[]>> => {
   const result = await request.get<Group[]>('group')
   return result
 }
 
+const create = async (group: CreateGroup): Promise<Response<Group>> => {
+  const result = await request.post<CreateGroup, Group>('group', group)
+  return result
+}
+
 export const groupsApi = {
   getAll,
+  create,
 }
