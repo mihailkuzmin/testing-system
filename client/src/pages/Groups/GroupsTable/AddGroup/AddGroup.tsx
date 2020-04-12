dd0f596b1bfd94cd92a0e3d2cd2e36505ad3ed20
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-12 10:26:47 +0300

    implement group creation in UI

--- client/src/pages/Groups/GroupsTable/AddGroup/AddGroup.tsx
+++ client/src/pages/Groups/GroupsTable/AddGroup/AddGroup.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
+import { useStore } from 'effector-react'
 import {
   PrimaryButton as Add,
   SecondaryButton as Cancel,
 } from '../../../../components/Buttons'
 import { MappedInput } from '../../../../components/Inputs'
+import { Linear } from '../../../../components/Loaders'
 import { addForm, addModal } from '../../model'
 import { AddForm } from '../../model/typings'
+import { Status } from '../../../../typings'
 import styles from './AddGroup.module.css'
 
 export const AddGroup = () => {
+  const createGroupStatus = useStore(addForm.$createGroupStatus)
+  const isPending = createGroupStatus === Status.Pending
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    addForm.createGroup()
+  }
+
   return (
     <div className={styles.addGroup}>
-      <h3>Добавить группу</h3>
-      <form noValidate autoComplete='off'>
+      <form onSubmit={onSubmit} noValidate autoComplete='off'>
+      <h3 className={styles.title}>Добавить группу</h3>
         <div className={styles.fields}>
           <MappedInput<AddForm>
             name='name'
@@ -21,11 +32,19 @@ export const AddGroup = () => {
             onChange={addForm.fieldValueChange}
           />
         </div>
+        {isPending ? (
+        <div className={styles.loader}>
+          <Linear />
+        </div>
+      ) : (
+        <div className={styles.actions}>
+          <Add type='submit' onClick={addForm.createGroup}>
+            Добавить
+          </Add>
+          <Cancel onClick={addModal.closeAddModal}>Отменить</Cancel>
+        </div>
+      )}
       </form>
-      <div className={styles.actions}>
-        <Add onClick={addModal.closeAddModal}>Добавить</Add>
-        <Cancel onClick={addModal.closeAddModal}>Отменить</Cancel>
-      </div>
     </div>
   )
 }
