7e9b2936ff81f2cbef0d0d52f7c976ee07abc530
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-12 21:34:54 +0300

    add deleteGroupFx effect

--- client/src/pages/Groups/model/groupsTable/effects.ts
+++ client/src/pages/Groups/model/groupsTable/effects.ts
@@ -1,4 +1,6 @@
+import { createEffect } from 'effector'
 import { createReEffect } from 'effector-reeffect'
 import { groupsApi } from '../../../../api'
 
 export const getGroupsFx = createReEffect({ handler: groupsApi.getAll })
+export const deleteGroupFx = createEffect({ handler: groupsApi.deleteById })
