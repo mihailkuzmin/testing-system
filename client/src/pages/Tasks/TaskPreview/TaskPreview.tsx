46dff20794c13c894a6ace17c0256d331e431cf7
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-25 22:10:31 +0300

    improve task preview layout

--- client/src/pages/Tasks/TaskPreview/TaskPreview.tsx
+++ client/src/pages/Tasks/TaskPreview/TaskPreview.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useStore } from 'effector-react'
 import { PageLoader, PageError } from '../../../components/Loaders'
+import { Paper } from '../../../components'
 import { PreviewPage, task } from '../model/taskPreview'
+import styles from './TaskPreview.module.css'
 
 type TaskPreviewProps = {
   id: number
@@ -22,9 +24,12 @@ export const TaskPreview = ({ id }: TaskPreviewProps) => {
   }
 
   return (
-    <div>
-      <p>Task {kekTask?.name} preview</p>
-      <div dangerouslySetInnerHTML={{ __html: kekTask?.description ?? '' }}></div>
-    </div>
+    <Paper className={styles.preview}>
+      <h2>Задание "{kekTask?.name}"</h2>
+      <div className={styles.description}>
+        <span>Описание</span>
+        <div dangerouslySetInnerHTML={{ __html: kekTask?.description ?? '' }}></div>
+      </div>
+    </Paper>
   )
 }
