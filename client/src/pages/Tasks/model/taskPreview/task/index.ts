9a0e6e9eb8d59f9bdf2268090b4a8295372c2242
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-25 22:32:03 +0300

    fixed race condition in Task preview page

--- client/src/pages/Tasks/model/taskPreview/task/index.ts
+++ client/src/pages/Tasks/model/taskPreview/task/index.ts
@@ -4,6 +4,7 @@ import { getTaskFx } from './effects'
 import { PreviewPage } from '../page'
 
 forward({ from: PreviewPage.open, to: getTaskFx })
+forward({ from: PreviewPage.close, to: getTaskFx.cancel })
 
 $task.on(getTaskFx.doneData, (_, { payload }) => payload)
 $task.reset(PreviewPage.close)
