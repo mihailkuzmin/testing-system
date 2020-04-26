3481210c12aad68164017a4b8e997f752b2294d1
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-04-26 11:31:08 +0300

    add payload & message typings

--- src/controllers/task.controller/typings/messages.ts
+++ src/controllers/task.controller/typings/messages.ts
@@ -1,5 +1,6 @@
 export const Create = 'Выполнено'
 export const GetAll = ''
 export const GetById = ''
+export const GetTestsById = ''
 export const Update = 'Выполнено'
 export const RemoveById = 'Выполнено'
--- src/controllers/task.controller/typings/payloads.ts
+++ src/controllers/task.controller/typings/payloads.ts
@@ -1,7 +1,8 @@
-import { ITask } from '../../../typings/task'
+import { ITask, Test } from '../../../typings/task'
 
 export interface Create extends ITask {}
 export interface GetAll extends Array<ITask> {}
 export interface GetById extends ITask {}
+export type GetTestsById = Test[]
 export interface Update extends ITask {}
 export interface RemoveById extends ITask {}
