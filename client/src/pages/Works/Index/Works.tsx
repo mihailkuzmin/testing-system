4f6f407f3346fd0d5077ffcb491cc912d307ed4a
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-04 12:50:51 +0300

    works page refactoring

--- client/src/pages/Works/Index/Works.tsx
+++ client/src/pages/Works/Index/Works.tsx
@@ -1,4 +1,21 @@
 import React from 'react'
+import { useStore } from 'effector-react'
+import { PageLoader, PageError } from '../../../components/Loaders'
+import { WorksPage } from '../model/index/page'
 import { WorksTable } from './WorksTable'
 
-export const Works = () => <WorksTable />
+export const Works = () => {
+  React.useEffect(WorksPage.onMount, [])
+
+  const { isLoading, isFail } = useStore(WorksPage.$status)
+
+  if (isLoading) {
+    return <PageLoader />
+  }
+
+  if (isFail) {
+    return <PageError />
+  }
+
+  return <WorksTable />
+}
