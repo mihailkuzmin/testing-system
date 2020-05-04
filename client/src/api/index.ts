de7d01a22fdfc250dd5990d866e875ad9deb21d9
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-04 12:50:07 +0300

    add worksApi

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,3 +1,4 @@
 export { usersApi } from './users'
 export { groupsApi } from './groups'
 export { tasksApi } from './tasks'
+export { worksApi } from './works'
---
+++ client/src/api/works/index.ts
@@ -0,0 +1,1 @@
+export { worksApi } from './works'
