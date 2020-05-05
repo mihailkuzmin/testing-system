4d47ec49deb7386423aaa48e70bdda2859fd0a0f
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-05 23:25:54 +0300

    reset stores on page unmount

--- client/src/pages/Works/model/addWork/addForm/index.ts
+++ client/src/pages/Works/model/addWork/addForm/index.ts
@@ -8,8 +8,10 @@ forward({ from: AddWorkPage.open, to: [getTasksFx, getTopicsFx] })
 forward({ from: AddWorkPage.close, to: [getTasksFx.cancel, getTopicsFx.cancel] })
 
 $tasks.on(getTasksFx.doneData, (_, { payload }) => payload)
+$tasks.reset(AddWorkPage.close)
 
 $topics.on(getTopicsFx.doneData, (_, { payload }) => payload)
+$topics.reset(AddWorkPage.close)
 
 $selectedTopic.on(getTopicsFx.doneData, (_, { payload }) => {
   if (payload.length) {
@@ -18,11 +20,13 @@ $selectedTopic.on(getTopicsFx.doneData, (_, { payload }) => {
   }
 })
 $selectedTopic.on(topicChange, (_, topicId) => topicId)
+$selectedTopic.reset(AddWorkPage.close)
 
 const $filteredTasks = sample({
   source: $tasks,
   clock: $selectedTopic,
   fn: (tasks, topicId) => tasks.filter((task) => task.topic.id === topicId),
 })
+$filteredTasks.reset(AddWorkPage.close)
 
 export const addForm = { $filteredTasks, $selectedTasks, $topics, $selectedTopic, topicChange }
