012dd89d810c1658e54f2ffaa9e5c1255d13d396
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-09 17:09:24 +0300

    add Request type

--- src/common/typings/index.ts
+++ src/common/typings/index.ts
@@ -1,4 +1,6 @@
-export type Response<T> = {
-  payload: T
-  message: string
+export type Request = {
+  get: <R>(url: string) => Promise<Response<R>>
+  post: <P, R>(url: string, payload?: P) => Promise<Response<R>>
+  put: <P, R>(url: string, payload?: P) => Promise<Response<R>>
+  delete: <P, R>(url: string, payload?: P) => Promise<Response<R>>
 }
