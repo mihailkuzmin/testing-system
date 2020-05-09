836a2f3b0b885ddd39098470e28c0dc7b8a423b9
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-09 11:02:22 +0300

    fix webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,12 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
     app: './src/client/index.tsx',
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/client/index.html'),
     }),
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,17 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [new BundleAnalyzerPlugin(), new CleanWebpackPlugin()],
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 })
