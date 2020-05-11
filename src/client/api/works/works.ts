578856b9967dc54f801f007fd0d311c0ca217d13
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-11 19:55:01 +0300

    add .getTasksOfWorkPreviews() method

--- src/client/api/works/works.ts
+++ src/client/api/works/works.ts
@@ -1,9 +1,15 @@
 import { Response } from '@common/typings'
 import { WorkId, Work, CreateWork } from '@common/typings/work'
 import { request } from '../request'
+import { TaskPreview } from '@common/typings/task'
 
 const getAll = async (): Promise<Response<Work[]>> => {
   const result = await request.get<Work[]>('work')
+  return result
+}
+
+const getTasksOfWorkPreviews = async (id: WorkId): Promise<Response<TaskPreview[]>> => {
+  const result = await request.get<TaskPreview[]>(`work/${id}/tasks`)
   return result
 }
 
@@ -25,6 +31,7 @@ const create = async (work: CreateWork): Promise<Response<void>> => {
 export const worksApi = {
   getAll,
   getById,
+  getTasksOfWorkPreviews,
   deleteById,
   create,
 }
