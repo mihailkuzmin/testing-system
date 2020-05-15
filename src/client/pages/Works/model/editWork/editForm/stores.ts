e80b004e213d65b28c2aeb776394afe76d9113b6
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-15 14:28:41 +0300

    added $id store

--- src/client/pages/Works/model/editWork/editForm/stores.ts
+++ src/client/pages/Works/model/editWork/editForm/stores.ts
@@ -1,11 +1,13 @@
 import { createStore } from 'effector'
 import { Task, Topic } from '@common/typings/task'
+import { WorkId } from '@common/typings/work'
 
 export const $tasks = createStore<Task[]>([])
 export const $topics = createStore<Topic[]>([])
 export const $selectedTopic = createStore<Topic | null>(null)
 
 export const $selectedTasks = createStore<Task[]>([])
+export const $id = createStore<WorkId | null>(null)
 export const $name = createStore('')
 export const $openAt = createStore(new Date())
 export const $closeAt = createStore(new Date())
