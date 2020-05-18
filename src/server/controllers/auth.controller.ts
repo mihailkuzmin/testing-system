b20905d64c696e6f52a6973b6f68886eca90eba6
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-18 13:44:32 +0300

    added stubs

--- src/server/controllers/auth.controller.ts
+++ src/server/controllers/auth.controller.ts
@@ -1,14 +1,16 @@
 import { Controller } from '@typings'
 
 export const authController: Controller = (app, options, done) => {
-  app.post('/signin', async (request, reply) => {
-    const { login, password } = request.body
-
-    reply.send({ login, password })
+  app.post('/login', async (request, reply) => {
+    reply.send()
   })
 
   app.get('/check', async (request, reply) => {
-    reply.send({ ok: true })
+    reply.send()
+  })
+
+  app.get('/logout', async (request, reply) => {
+    reply.send()
   })
 
   done()
