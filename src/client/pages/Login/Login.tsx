e827e801010472cfb54cd56a5b238c927d0ec405
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-18 11:18:34 +0300

    added Login page layout

--- src/client/pages/Login/Login.tsx
+++ src/client/pages/Login/Login.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Paper } from '@components'
+import { Input } from '@components/Inputs'
+import { PrimaryButton } from '@components/Buttons'
+import styles from './Login.module.css'
 
 export const Login = () => {
   return (
-    <div>
-      <p>Login)</p>
-    </div>
+    <Paper className={styles.login}>
+      <form className={styles.loginForm}>
+        <h2>Вход</h2>
+        <Input label='Логин' />
+        <Input type='password' label='Пароль' />
+        <PrimaryButton>Войти</PrimaryButton>
+      </form>
+    </Paper>
   )
 }
