3bcfa118942e8a75edc60bbed2f000ae76fb03d3
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-19 18:46:57 +0300

    added Main page stub

--- src/client/pages/Main/Main.tsx
+++ src/client/pages/Main/Main.tsx
@@ -1,3 +1,17 @@
 import React from 'react'
+import { useStore } from 'effector-react'
+import { auth } from '@model'
+import { Paper } from '@components'
+import styles from './Main.module.css'
 
-export const Main = () => <div>Main</div>
+export const Main = () => {
+  const user = useStore(auth.$user)
+
+  return (
+    <Paper className={styles.main}>
+      <p>Пользователь: {`${user?.lastName} ${user?.firstName} ${user?.patronymic}`}</p>
+      <p>Группа: {user?.group.name}</p>
+      <p>Номер зачетной книжки: {user?.bookNumber}</p>
+    </Paper>
+  )
+}
