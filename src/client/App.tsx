008eeda9056f6b394aae67869c7d1efb8674bafd
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-19 11:58:33 +0300

    added authentication check on App startup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useStore } from 'effector-react'
 import { useRoutes } from 'hookrouter'
 import { SnackbarProvider } from 'notistack'
 import { Navigation, NavLink } from '@components/Navigation'
 import * as Icons from '@components/Icons'
 import { Header, Layout, Notifier } from '@components'
 import { routes } from '@routes'
+import { Login } from '@pages'
+import { auth } from '@model'
 
 function App() {
+  React.useEffect(auth.check, [])
+  const isAuth = useStore(auth.$isAuth)
   const pages = useRoutes(routes)
+
+  if (!isAuth) {
+    return <Login />
+  }
 
   return (
     <div className='App'>
@@ -16,7 +25,6 @@ function App() {
         <Header>
           <Navigation>
             <NavLink href='/' strict text='Главная' icon={Icons.Home} />
-            <NavLink href='/login' text='Вход' icon={Icons.Login} />
             <NavLink href='/groups' text='Группы' icon={Icons.Groups} />
             <NavLink href='/users' text='Пользователи' icon={Icons.Users} />
             <NavLink href='/tasks' text='Задания' icon={Icons.Tasks} />
