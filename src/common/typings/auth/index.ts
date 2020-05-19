f684a43d8d218e357eb354e0519cccd655ee3e1f
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-19 18:45:51 +0300

    added typings

--- src/common/typings/auth/index.ts
+++ src/common/typings/auth/index.ts
@@ -1,5 +1,13 @@
+import { Student } from '../student'
+
 export type UserId = number
 
 export type Credentials = { login: string; password: string }
 
-export type UserInfo = { id: UserId; name: string }
+export type UserInfo = Student
+
+export type AuthInfo = {
+  user?: UserInfo
+  error?: boolean
+  message?: string
+}
