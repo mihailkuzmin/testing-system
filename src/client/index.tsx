e97eff623f0a8a71436b2ef0b01dc3a8f776913c
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-19 18:47:32 +0300

    added auth check on startup

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { auth } from '@model'
 import App from './App'
 import './index.css'
+
+auth.checkAuth()
 
 ReactDOM.render(<App />, document.getElementById('root'))
