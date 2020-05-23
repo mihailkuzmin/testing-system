ba0229691541a7432a09f9115df624dce1232264
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-23 21:40:16 +0300

    refactored user.controller with allowFor hook

--- src/server/controllers/user.controller.ts
+++ src/server/controllers/user.controller.ts
@@ -1,50 +1,76 @@
-import { User, CreateUser, UpdateUser, UserId } from '@common/typings/user'
+import { CreateUser, Roles, UpdateUser, User, UserId } from '@common/typings/user'
 import { Response } from '@common/typings'
 import { Controller } from '@typings'
 import { UserRepository } from '@repositories'
+import { allowFor } from '@hooks'
 
 export const userController: Controller = (app, options, done) => {
-  app.get('/', async (request, reply) => {
-    const result = await UserRepository.getAll()
+  app.route({
+    method: 'GET',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const result = await UserRepository.getAll()
 
-    const response: Response<User[]> = { payload: result }
-    reply.send(response)
+      const response: Response<User[]> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.get('/:id', async (request, reply) => {
-    const id: UserId = request.params.id
+  app.route({
+    method: 'GET',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: UserId = request.params.id
 
-    const result = await UserRepository.getById(id)
+      const result = await UserRepository.getById(id)
 
-    const response: Response<User> = { payload: result }
-    reply.send(response)
+      const response: Response<User> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.post('/', async (request, reply) => {
-    const newStudent: CreateUser = request.body
+  app.route({
+    method: 'POST',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const newStudent: CreateUser = request.body
 
-    await UserRepository.create(newStudent)
+      await UserRepository.create(newStudent)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.put('/', async (request, reply) => {
-    const user: UpdateUser = request.body
+  app.route({
+    method: 'PUT',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const user: UpdateUser = request.body
 
-    await UserRepository.update(user)
+      await UserRepository.update(user)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.delete('/:id', async (request, reply) => {
-    const id: UserId = request.params.id
+  app.route({
+    method: 'DELETE',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: UserId = request.params.id
 
-    await UserRepository.removeById(id)
+      await UserRepository.removeById(id)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
   done()
