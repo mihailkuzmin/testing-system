78d702919b79fb46b6d4b409b4d2b12d7b34e9f6
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-23 19:05:56 +0300

    refactored group.controller with allowFor hook

--- src/server/controllers/group.controller.ts
+++ src/server/controllers/group.controller.ts
@@ -1,50 +1,77 @@
-import { GroupRepository } from '@repositories'
-import { Group, CreateGroup, UpdateGroup, GroupId } from '@common/typings/group'
+import { CreateGroup, Group, GroupId, UpdateGroup } from '@common/typings/group'
 import { Response } from '@common/typings'
 import { Controller } from '@typings'
+import { GroupRepository } from '@repositories'
+import { allowFor } from '@hooks'
+import { Roles } from '@common/typings/user'
 
 export const groupController: Controller = (app, options, done) => {
-  app.get('/', async (request, reply) => {
-    const result = await GroupRepository.getAll()
+  app.route({
+    method: 'GET',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const result = await GroupRepository.getAll()
 
-    const response: Response<Group[]> = { payload: result }
-    reply.send(response)
+      const response: Response<Group[]> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.get('/:id', async (request, reply) => {
-    const id: GroupId = request.params.id
+  app.route({
+    method: 'GET',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: GroupId = request.params.id
 
-    const result = await GroupRepository.getById(id)
+      const result = await GroupRepository.getById(id)
 
-    const response: Response<Group> = { payload: result }
-    reply.send(response)
+      const response: Response<Group> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.post('/', async (request, reply) => {
-    const newGroup: CreateGroup = request.body
+  app.route({
+    method: 'POST',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const newGroup: CreateGroup = request.body
 
-    await GroupRepository.create(newGroup)
+      await GroupRepository.create(newGroup)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.delete('/:id', async (request, reply) => {
-    const id: GroupId = request.params.id
+  app.route({
+    method: 'DELETE',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: GroupId = request.params.id
 
-    await GroupRepository.removeById(id)
+      await GroupRepository.removeById(id)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.put('/', async (request, reply) => {
-    const group: UpdateGroup = request.body
+  app.route({
+    method: 'PUT',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const group: UpdateGroup = request.body
 
-    await GroupRepository.update(group)
+      await GroupRepository.update(group)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
   done()
