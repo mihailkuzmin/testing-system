409dc66703715683bd591124c8a1900d8d6f1eef
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-23 19:14:07 +0300

    refactored task.controller with allowFor hook

--- src/server/controllers/task.controller.ts
+++ src/server/controllers/task.controller.ts
@@ -1,80 +1,117 @@
 import { TaskRepository } from '@repositories'
 import { Response } from '@common/typings'
 import { Controller } from '@typings'
-import { Task, CreateTask, UpdateTask, TaskId, Topic, Test } from '@common/typings/task'
+import { CreateTask, Task, TaskId, Test, Topic, UpdateTask } from '@common/typings/task'
+import { allowFor } from '@hooks'
+import { Roles } from '@common/typings/user'
 
 export const taskController: Controller = (app, options, done) => {
-  app.get('/', async (request, reply) => {
-    const fieldsForExclude: Array<keyof Task> = request.query.exclude ?? []
+  app.route({
+    method: 'GET',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const fieldsForExclude: Array<keyof Task> = request.query.exclude ?? []
 
-    const result = await TaskRepository.getAll()
+      const result = await TaskRepository.getAll()
 
-    for (const field of fieldsForExclude) {
-      for (const task of result) {
+      for (const field of fieldsForExclude) {
+        for (const task of result) {
+          delete task[field]
+        }
+      }
+
+      const response: Response<Task[]> = { payload: result }
+      reply.send(response)
+    },
+  })
+
+  app.route({
+    method: 'GET',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const fieldsForExclude: Array<keyof Task> = request.query.exclude ?? []
+      const id: TaskId = request.params.id
+
+      const task = await TaskRepository.getById(id)
+
+      for (const field of fieldsForExclude) {
         delete task[field]
       }
-    }
 
-    const response: Response<Task[]> = { payload: result }
-    reply.send(response)
+      const response: Response<Task> = { payload: task }
+      reply.send(response)
+    },
   })
 
-  app.get('/:id', async (request, reply) => {
-    const fieldsForExclude: Array<keyof Task> = request.query.exclude ?? []
-    const id: TaskId = request.params.id
+  app.route({
+    method: 'POST',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator, Roles.Moderator]),
+    handler: async (request, reply) => {
+      const newTask: CreateTask = request.body
 
-    const task = await TaskRepository.getById(id)
+      await TaskRepository.create(newTask)
 
-    for (const field of fieldsForExclude) {
-      delete task[field]
-    }
-
-    const response: Response<Task> = { payload: task }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.post('/', async (request, reply) => {
-    const newTask: CreateTask = request.body
+  app.route({
+    method: 'GET',
+    url: '/:id/test',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: TaskId = request.params.id
 
-    await TaskRepository.create(newTask)
+      const tests = await TaskRepository.getTestsById(id)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<Test[]> = { payload: tests }
+      reply.send(response)
+    },
   })
 
-  app.get('/:id/test', async (request, reply) => {
-    const id: TaskId = request.params.id
+  app.route({
+    method: 'GET',
+    url: '/topic',
+    preValidation: allowFor([Roles.Administrator, Roles.Moderator]),
+    handler: async (request, reply) => {
+      const topics = await TaskRepository.getTopics()
 
-    const tests = await TaskRepository.getTestsById(id)
+      const response: Response<Topic[]> = { payload: topics }
 
-    const response: Response<Test[]> = { payload: tests }
-    reply.send(response)
+      reply.send(response)
+    },
   })
 
-  app.get('/topic', async (request, reply) => {
-    const topics = await TaskRepository.getTopics()
+  app.route({
+    method: 'PUT',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const task: UpdateTask = request.body
 
-    const response: Response<Topic[]> = { payload: topics }
+      await TaskRepository.update(task)
 
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.put('/', async (request, reply) => {
-    const task: UpdateTask = request.body
+  app.route({
+    method: 'DELETE',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: TaskId = request.params.id
 
-    await TaskRepository.update(task)
+      await TaskRepository.removeById(id)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
-  })
-
-  app.delete('/:id', async (request, reply) => {
-    const id: TaskId = request.params.id
-
-    await TaskRepository.removeById(id)
-
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
   done()
