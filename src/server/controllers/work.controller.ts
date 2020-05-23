6d2d3d274b825dbb74035479c6f7feb2fc2188d6
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-23 21:53:10 +0300

    refactored work.controller with allowFor hook

--- src/server/controllers/work.controller.ts
+++ src/server/controllers/work.controller.ts
@@ -1,76 +1,113 @@
-import { WorkRepository } from '@repositories'
-import { Response } from '@common/typings'
-import { Work, WorkId, CreateWork, UpdateWork } from '@common/typings/work'
+import { CreateWork, UpdateWork, Work, WorkId } from '@common/typings/work'
 import { Task, TaskId } from '@common/typings/task'
+import { Roles } from '@common/typings/user'
+import { Response } from '@common/typings'
 import { Controller } from '@typings'
+import { WorkRepository } from '@repositories'
+import { allowFor } from '@hooks'
 
 export const workController: Controller = (app, options, done) => {
-  app.get('/', async (request, reply) => {
-    const result = await WorkRepository.getAll()
+  app.route({
+    method: 'GET',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const result = await WorkRepository.getAll()
 
-    const response: Response<Work[]> = { payload: result }
-    reply.send(response)
+      const response: Response<Work[]> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.get('/:id', async (request, reply) => {
-    const id: WorkId = request.params.id
+  app.route({
+    method: 'GET',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: WorkId = request.params.id
 
-    const result = await WorkRepository.getById(id)
+      const result = await WorkRepository.getById(id)
 
-    const response: Response<Work> = { payload: result }
-    reply.send(response)
+      const response: Response<Work> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.get('/:id/task', async (request, reply) => {
-    const id: WorkId = request.params.id
-    const fieldsForExclude: Array<keyof Task> = request.query.exclude ?? []
+  app.route({
+    method: 'GET',
+    url: '/:id/task',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: WorkId = request.params.id
+      const fieldsForExclude: Array<keyof Task> = request.query.exclude ?? []
 
-    const result = await WorkRepository.getTasksOfWork(id)
+      const result = await WorkRepository.getTasksOfWork(id)
 
-    for (const field of fieldsForExclude) {
-      for (const task of result) {
-        delete task[field]
+      for (const field of fieldsForExclude) {
+        for (const task of result) {
+          delete task[field]
+        }
       }
-    }
 
-    const response: Response<Task[]> = { payload: result }
-    reply.send(response)
+      const response: Response<Task[]> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.get('/filterByTask/:id', async (request, reply) => {
-    const id: TaskId = request.params.id
+  app.route({
+    method: 'GET',
+    url: '/filterByTask/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: TaskId = request.params.id
 
-    const result = await WorkRepository.getWorksWithTask(id)
+      const result = await WorkRepository.getWorksWithTask(id)
 
-    const response: Response<Work[]> = { payload: result }
-    reply.send(response)
+      const response: Response<Work[]> = { payload: result }
+      reply.send(response)
+    },
   })
 
-  app.post('/', async (request, reply) => {
-    const newWork: CreateWork = request.body
+  app.route({
+    method: 'POST',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const newWork: CreateWork = request.body
 
-    await WorkRepository.create(newWork)
+      await WorkRepository.create(newWork)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.put('/', async (request, reply) => {
-    const updateWork: UpdateWork = request.body
+  app.route({
+    method: 'PUT',
+    url: '/',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const updateWork: UpdateWork = request.body
 
-    await WorkRepository.update(updateWork)
+      await WorkRepository.update(updateWork)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
-  app.delete('/:id', async (request, reply) => {
-    const id: WorkId = request.params.id
+  app.route({
+    method: 'DELETE',
+    url: '/:id',
+    preValidation: allowFor([Roles.Administrator]),
+    handler: async (request, reply) => {
+      const id: WorkId = request.params.id
 
-    await WorkRepository.removeById(id)
+      await WorkRepository.removeById(id)
 
-    const response: Response<void> = { message: 'Выполнено' }
-    reply.send(response)
+      const response: Response<void> = { message: 'Выполнено' }
+      reply.send(response)
+    },
   })
 
   done()
