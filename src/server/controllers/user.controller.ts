31b324fb96794406252ae02499d5937b2233ea30
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-24 11:31:44 +0300

    added /:id/works endpoint

--- src/server/controllers/user.controller.ts
+++ src/server/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { CreateUser, Roles, UpdateUser, User, UserId } from '@common/typings/user'
+import { Work } from '@common/typings/work'
 import { Response } from '@common/typings'
 import { Controller } from '@typings'
 import { UserRepository } from '@repositories'
@@ -27,6 +28,20 @@ export const userController: Controller = (app, options, done) => {
       const result = await UserRepository.getById(id)
 
       const response: Response<User> = { payload: result }
+      reply.send(response)
+    },
+  })
+
+  app.route({
+    method: 'GET',
+    url: '/:id/works',
+    preValidation: allowFor([Roles.Administrator, Roles.Moderator, Roles.Student]),
+    handler: async (request, reply) => {
+      const id: UserId = request.params.id
+
+      const result = await UserRepository.getAvailableWorksById(id)
+
+      const response: Response<Work[]> = { payload: result }
       reply.send(response)
     },
   })
