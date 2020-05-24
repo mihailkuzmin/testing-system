00ccf64643abfa6bec799aef8420ab760f72b277
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-24 11:41:39 +0300

    changed prefix to /user

--- src/client/api/users/users.ts
+++ src/client/api/users/users.ts
@@ -3,27 +3,27 @@ import { CreateUser, UpdateUser, User, UserId } from '@common/typings/user'
 import { request } from '../request'
 
 const getAll = async (): Promise<Response<User[]>> => {
-  const result = await request.get<User[]>('student')
+  const result = await request.get<User[]>('user')
   return result
 }
 
 const getById = async (id: UserId): Promise<Response<User>> => {
-  const result = await request.get<User>(`student/${id}`)
+  const result = await request.get<User>(`user/${id}`)
   return result
 }
 
 const create = async (user: CreateUser): Promise<Response<void>> => {
-  const result = await request.post<CreateUser, void>('student', user)
+  const result = await request.post<CreateUser, void>('user', user)
   return result
 }
 
 const deleteById = async (id: UserId): Promise<Response<void>> => {
-  const result = await request.delete<UserId, void>(`student/${id}`)
+  const result = await request.delete<UserId, void>(`user/${id}`)
   return result
 }
 
 const update = async (user: UpdateUser): Promise<Response<void>> => {
-  const result = await request.put<UpdateUser, void>('student', user)
+  const result = await request.put<UpdateUser, void>('user', user)
   return result
 }
 
