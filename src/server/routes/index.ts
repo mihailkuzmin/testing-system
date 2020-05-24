eb7268e17c25fc526064bea0522ae28f4643b580
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-05-24 11:40:19 +0300

    changed prefix to /user

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,7 +3,7 @@ import { Controller } from '@typings'
 
 export const routes: Controller = (app, options, done) => {
   app.register(controllers.workController, { prefix: '/work' })
-  app.register(controllers.userController, { prefix: '/student' })
+  app.register(controllers.userController, { prefix: '/user' })
   app.register(controllers.taskController, { prefix: '/task' })
   app.register(controllers.groupController, { prefix: '/group' })
   app.register(controllers.authController, { prefix: '/auth' })
